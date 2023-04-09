679e1e174cd8271faff12bf834cfb229e6d9d61e
LAPTOP-B0212VLT\User <kenziefubrianto@gmail.com>
2023-04-09 18:09:23 +0700

    no message

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Box } from '../util/Box'
+import classes from "./Book.module.css"
 export const Book = () => {
   return (
     <Box>
-        <div style={{ marginLeft:"50px" }}>
-            <div></div>
-            <div id='book-info'>
-                <span>4 hari lagi</span>
-                <div>
+        <div style={{ marginLeft:"50px", display:"flex",padding:'35px' }}>
+            <img sty width="150px" height="210" src="./assets/book.png" alt="" />
+            <div className={classes["book-info"]} id='book-info'>
+                <span className={classes["due-date"]}>4 hari lagi</span>
+                <div style={{ display:"flex",justifyContent:"space-between", alignItems:"center" }}>
                     <h2>Laut Bercerita</h2>
                     <button>Details</button>
                 </div>
